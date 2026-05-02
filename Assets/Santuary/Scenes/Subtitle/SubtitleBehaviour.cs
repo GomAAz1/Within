7322839329c3e5abeffb49ef058a33e9e1fb6f52
ssrrrfy <alostoramedo2008@gmail.com>
2026-05-02 16:14:39 +0200

    End cutscene Subtitles

--- Assets/Santuary/Scenes/Subtitle/SubtitleBehaviour.cs
+++ Assets/Santuary/Scenes/Subtitle/SubtitleBehaviour.cs
@@ -9,10 +9,29 @@ public class SubtitleBehaviour : PlayableBehaviour
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
         TextMeshProUGUI textMesh = playerData as TextMeshProUGUI;
+
         if (textMesh != null)
         {
+            // كتابة النص
             textMesh.text = subtitleText;
+
+            // التحكم في الشفافية (الفيد)
+            // info.weight بيوصل لـ 1 لما الكليب يشتغل، وصفر في الفراغات
             textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, info.weight);
+
+            // تكة إضافية: لو الشفافية صفر (يعني إحنا بره الكليب)، امسح النص
+            if (info.weight <= 0)
+            {
+                textMesh.text = "";
+            }
         }
+    }
+
+    // الدالة دي بتشتغل لما الخط الأبيض يخرج بره الكليب
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        // أول ما الكليب يخلص، بنصفر النص فوراً
+        var textMesh = playable.GetGraph().GetResolver() as TextMeshProUGUI;
+        if (textMesh != null) textMesh.text = "";
     }
 }
--- Assets/Santuary/Scenes/Subtitle/SubtitleClip.cs
+++ Assets/Santuary/Scenes/Subtitle/SubtitleClip.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class SubtitleClip
+public class SubtitleClip : PlayableAsset
 {
-    
+    [TextArea(3, 10)]
+    public string text;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<SubtitleBehaviour>.Create(graph);
+        SubtitleBehaviour behaviour = playable.GetBehaviour();
+        behaviour.subtitleText = text;
+        return playable;
+    }
 }
